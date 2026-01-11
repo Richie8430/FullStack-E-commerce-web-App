b4dad689649825fb07ddff1004df92c350068819
Richie8430 <rrkihongo843@gmail.com>
2026-01-11 14:41:52 +0300

    Converting html to react

--- e-commerceApp/src/App.jsx
+++ e-commerceApp/src/App.jsx
@@ -1,10 +1,17 @@
+import { Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
+import CheckOut from "./pages/CheckOutPage";
+import Orders from "./pages/OrdersPage";
+import TrackingPage from "./pages/TrackingPage";
 
 function App() {
   return (
-    <>
-      <HomePage />
-    </>
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="checkout" element={<CheckOut />} />
+      <Route path="orders" element={<Orders />} />
+      <Route path="tracking" element={<TrackingPage />} />
+    </Routes>
   );
 }
 
